4ef037c58b4a3d8cef9daaf95cf58af3bbdb6c46
john-smilga <johnsmilga@gmail.com>
2019-12-06 17:07:19 -0800

    navbar and cocktails complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <ul className="nav-links">
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/about">about</Link>
-        </li>
-        <li>
-          <Link to="/random">random</Link>
-        </li>
-      </ul>
+      <div className="nav-center">
+        <img src={logo} alt="cocktail db logo" className="logo" />
+        <ul className="nav-links">
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+          <li>
+            <Link to="/random">random</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
